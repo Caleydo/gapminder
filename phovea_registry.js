4aa7d003e6b4df76c1a134546d8b90b601d86ec6
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-02-07 13:33:02 +0100

    remove superfluous dependencies

--- phovea_registry.js
+++ phovea_registry.js
@@ -10,10 +10,6 @@ import {register} from 'phovea_core/src/plugin';
  * build a registry by registering all phovea modules
  */
 //other modules
-import 'phovea_core/phovea_registry.js';
-import 'phovea_vis/phovea_registry.js';
-import 'phovea_d3/phovea_registry.js';
-import 'phovea_ui/phovea_registry.js';
 import 'phovea_clue/phovea_registry.js';
 //self
 register('gapminder',require('./phovea.js'));
