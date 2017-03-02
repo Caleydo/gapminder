b531e59beee1fdba852f24c73facba796636b082
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-03-02 11:15:06 +0100

    add additional datasets: main_religion and fertility

---
+++ data/raw/processing.R
@@ -0,0 +1,17 @@
+continent <- read.delim("continent.txt")
+countries= rownames(countries)
+
+#religion
+main_religion <- read.delim("main_religion.csv")
+# order and select the countries we need
+religions = main_religion[match(countries, main_religion$Entity),]
+write.table(religions, "../main_religions.txt", row.names=F, sep='\t', quote=F)
+summary(religions$Group)
+
+
+# fertility
+total_fertility <- read.delim("total_fertility.csv", stringsAsFactors=FALSE)
+fertility = total_fertility[match(countries, total_fertility$Total.fertility.rate),]
+colnames(fertility) = c('fertility', 1800:2015)
+write.table(fertility, "../fertility.txt", row.names=F, sep='\t', quote=F)
+summary(fertility)
